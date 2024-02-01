be0c4bfd6e74096ab47b2476dabb1831c81c37dc
P1su <pissssssu@naver.com>
2024-02-01 22:57:14 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css'; // 페이지의 디자인이 담겨 있음
+import { useState } from 'react';//리액트 제공 기본 함수
 //이 부분에서 내용을 설정
 
 //사용자 정의 태그를 만들기 위해서는 함수를 생성..반드시 대문자로 시작
@@ -43,21 +44,48 @@ function Article(props){
 }
 
 function App() {//다음 함수가 화면을 구성하는 것임 
+ // const _mode = useState('WELCOME'); // 지역변수인 mode를 usestate를 통해서 상태로 만듦
+ // const mode = _mode[0];
+ // const setMode = _mode[1];
+
+  const [mode, setMode] = useState('WELCOME');//축약형
+  const [id, setId] = useState(null);
+
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'},
   ]
 
+  let content = null;
+
+  if(mode === 'WELCOME'){
+    content = <Article title = "Welcome" body = "Hello, Web"></Article>
+  }else if(mode === 'READ'){
+    let title, body = null;
+
+    for(let i = 0 ; i < topics.length ; i++){
+      if(topics[i].id == id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+
+    content = <Article title = {title} body = {body}></Article>
+  }
+
   return (
     <div className="App">
       <Header title = "REACT" onChangeMode = {()=>{
-        alert('Header')
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics = {topics} onChangeMode={(id)=>{
-        alert(id);
+      
+      <Nav topics = {topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id)
       }} ></Nav>
-      <Article title = "Welcome" body = "Hello, eact!"></Article>
+
+      {content}
     </div>
 
   );//nav는 이동하는 영역 article은 본문 표시 영역
