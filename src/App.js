bee647d44beca16fdcfed3d2d269c5ffe4be7da9
P1su <pissssssu@naver.com>
2024-02-01 22:24:40 +0900

    props & event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; // 페이지의 디자인이 담겨 있음
+//이 부분에서 내용을 설정
 
-function App() {
+//사용자 정의 태그를 만들기 위해서는 함수를 생성..반드시 대문자로 시작
+function Header(props){//<컴포넌트> == 흔히 말하는 사용자 정의 태그!
+  console.log('props',props,props.title)
+
+  return <header> 
+    <h1><a href='/' onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1> 
+  </header>
+}
+
+function Nav(props){
+  const lis = []
+  for( let i = 0 ; i < props.topics.length; i++){
+    let t = props.topics[i]
+    lis.push(<li key = {t.id}>
+
+      <a id = {t.id} href={'/read/' + t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}> {t.title} </a>
+
+    </li>)
+
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
+function App() {//다음 함수가 화면을 구성하는 것임 
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title = "REACT" onChangeMode = {()=>{
+        alert('Header')
+      }}></Header>
+      <Nav topics = {topics} onChangeMode={(id)=>{
+        alert(id);
+      }} ></Nav>
+      <Article title = "Welcome" body = "Hello, eact!"></Article>
     </div>
-  );
+
+  );//nav는 이동하는 영역 article은 본문 표시 영역
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//여기에 적힌대로 동작한다.
+//App태그는 App.js에서 불러온 것임
+//전역적인 설정들이 들어가 있음
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(//아이디 값이 root인 태그로 렌더링
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
 
